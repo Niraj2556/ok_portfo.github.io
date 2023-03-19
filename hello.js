3bb6bda10748a3055f1a189093f45624e597b7e7
Niraj2556 <Nirajgupta54180@gmail.com>
2023-03-19 20:17:42 +0530

    portfolio

---
+++ hello.js
@@ -0,0 +1,45 @@
+// var ok = document.querySelectorAll("#hello");
+
+// var haan = document.querySelectorAll("#baap");
+
+// ok.forEach(o=>{
+//     o.addEventListener("click",(e)=>{
+//         // e.target.style.background="grey";
+//         e.target.innerHTML="Hello"
+//     })
+// })
+// function myFunction() {
+//     document.getElementById("baap").style.background = "black";
+//   }
+
+
+
+// function changeColor(color) {
+//     var ok = document.querySelectorAll(".main").toggleClass("nightmode");
+//     ok.body.style.background = color;
+// }
+  
+// function gfg_Run() {
+//     changeColor('yellow');
+// }
+
+
+
+// $(function() {
+//     $('.main').onClick(function gfg_Run() {
+//       $('body').toggleClass('nightmode');
+//   })
+// });
+
+
+
+
+// el_up.innerHTML = str;
+          
+// function changeColor(color) {
+//     document.body.style.background = color;
+// }
+  
+// function gfg_Run() {
+//     changeColor('yellow');
+// }     
